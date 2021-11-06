6665536aeb56554f2f318a977fef622d2f37e6a1
aleachaval <80827601+aleachaval@users.noreply.github.com>
2021-11-05 23:35:57 -0300

    Update Parcial.cpp

--- Parcial.cpp
+++ Parcial.cpp
@@ -17,32 +17,29 @@ Funcion main
 Funcion principal que se encarga de cargar los argumentos y comandar el programa
 */
 int main(int argc, char **argv) {
-    string path = "D:\\User\\Desktop\\Coding\\Parcial II_definitivo\\Covid19Casos1000.csv";
+    string path = "D:\\User\\Desktop\\Coding\\Parcial II_definitivo\\Covid19Casos-100000.csv";
     vector<vector<string>> nueva_tabla = AbrirCSV(path);
     for (int i = 1; i < argc; i++) {
         switch (str2int(argv[i])){
             case str2int("-estad"):
                 estadisticas(&nueva_tabla);
                 break;
-                case str2int("-p_casos"):
-                    casosSegunProvincias(atoi(argv[i + 1]), &nueva_tabla);
-                    break;
-                    case str2int("-p_muertes"):
-                        fallecidos_provincias(atoi(argv[i + 1]), &nueva_tabla);
-                        break;
-                        case str2int ("-casos_edad"):
-                            casos_edad(atoi(argv[i + 1]), &nueva_tabla);
-                            break;
-                        case str2int ("-casos_cui"):
-                            casos_cui(&nueva_tabla);
-                            break;
-                            default:
-                                break;
+            case str2int("-p_casos"):
+                casosSegunProvincias(atoi(argv[i + 1]), &nueva_tabla);
+                break;
+            case str2int("-p_muertes"):
+                fallecidos_provincias(atoi(argv[i + 1]), &nueva_tabla);
+                break;
+            case str2int ("-casos_edad"):
+                casos_edad(atoi(argv[i + 1]), &nueva_tabla);
+                break;
+            case str2int ("-casos_cui"):
+                casos_cui(atoi(argv[i + 1]),&nueva_tabla);
+                break;
+            default:
+                break;
         }
     }
 
     return 0;
 }
-
-
-
